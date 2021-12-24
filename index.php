61e5e96647764456b14710f0599b151f104d3b22
Sixcha <paulbora_akcan@hotmail.com>
2021-12-24 15:39:35 +0100

    Fixed routes, moved some functions to separate files and created french about page

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 require_once "config/config.php";
 require_once "lib/lib.php";
 
+session_start();
+
 $dbh = dbConnexion();
 $sth = $dbh ->prepare( "SELECT * FROM literature ORDER BY id DESC");
 $sth->execute();
 $articles = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-if(array_key_exists('lang', $_GET)){
-    switch ($_GET["lang"]){
-        case "fr":
-            include "phtml/homefr.phtml";
-        break;
-
-        default:
-            include "phtml/home.phtml";
+if(array_key_exists('page', $_GET)){
+    if(array_key_exists('lang', $_GET)){
+        include "phtml/".$_GET['page'].$_GET['lang'].".phtml";
+        $_SESSION['currentPage'] = $_GET['page'];
+    }
+    else{
+        header("Location: index.php?page=".$_SESSION['currentPage']."&lang=eng");
+        exit;
     }
 }
 else{
-    header("Location: index.php?lang=eng");
-    exit;
+    $_SESSION['currentPage'] = 'home';
+    header("Location: index.php?page=".$_SESSION['currentPage']."&lang=eng");
 }
 
-
-?>
